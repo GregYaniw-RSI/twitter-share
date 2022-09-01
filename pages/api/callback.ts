b39b05bb9aac13aead434f64cea628ec86fd29ca
GregYaniw-RSI <99285874+GregYaniw-RSI@users.noreply.github.com>
2022-09-01 12:19:43 -0400

    Adding twitter sharing files

---
+++ pages/api/callback.ts
@@ -0,0 +1,64 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import cookie from 'cookie';
+
+import mockImage from '../../image';
+
+import TwitterApi from 'twitter-api-v2';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { oauth_token, oauth_verifier } = req.query;
+  
+  const cookies = cookie.parse(req.headers.cookie || '');
+  const oauth_token_secret = cookies.oauth_token_secret;
+
+  console.log('CALLBACK');
+  console.log(oauth_token);
+
+  if (!oauth_token || !oauth_verifier) {
+    return res.status(400).send('You denied the app or your session expired!');
+  }
+
+  const clientOptions = {
+    appKey: process.env.TWITTER_CONSUMER_KEY,
+    appSecret: process.env.TWITTER_CONSUMER_SECRET,
+    accessToken: oauth_token,
+    accessSecret: oauth_token_secret,
+  };
+
+  console.log(clientOptions);
+
+  const twitterClient = new TwitterApi(clientOptions as any);
+
+  try {
+    const { client, userId } = await twitterClient.login(oauth_verifier as string);
+
+    console.log(`Trying to tweet on behalf of ${userId}`);
+
+    const mediaId = await client.v1.uploadMedia(Buffer.from(mockImage), {
+      type: 'png',
+      mimeType: 'image/png',
+    });
+    await client.v1.tweet('Hello there!', {
+      media_ids: mediaId,
+    });
+  } catch(error) {
+    console.log(error);
+
+    res.status(401).json({
+      status: 'FAILURE'
+    });
+
+    return;
+  }
+
+  res.status(200).json({
+    oauthToken: req.query.oauth_token,
+    oauthVerifier: req.query.oauth_verifier,
+    accessToken: oauth_token,
+    accessSecret: oauth_token_secret,
+  });
+}
---
+++ pages/api/hello.ts
@@ -0,0 +1,31 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import TwitterApi from 'twitter-api-v2';
+import cookie from 'cookie';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const twitterClient = new TwitterApi({
+    appKey: process.env.TWITTER_CONSUMER_KEY || '',
+    appSecret: process.env.TWITTER_CONSUMER_SECRET || '',
+  });
+
+  const {
+    url,
+    oauth_token,
+    oauth_token_secret
+  } = await twitterClient.generateAuthLink(process.env.TWITTER_CALLBACK_URL, { linkMode: 'authorize' });
+
+  console.log('HELLO');
+  console.log(oauth_token);
+
+  res.setHeader('Set-Cookie',
+    cookie.serialize('oauth_token_secret', String(oauth_token_secret), {
+      maxAge: 60 * 60 * 24 * 7 // 1 week
+    })
+  );
+
+  res.redirect(url);
+}
