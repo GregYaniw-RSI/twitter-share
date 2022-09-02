e2f4b4d0bffe6c439a8747660c3ed01d9a3c7592
Greg Yaniw <yaniw@rushstreetinteractive.com>
2022-09-02 12:53:10 -0400

    test

--- pages/api/callback.ts
+++ pages/api/callback.ts
@@ -40,9 +40,6 @@ export default async function handler(
     await client.v1.tweet(cookieContent.text, {
       media_ids: mediaId,
     });
-
-    window.close();
-
   } catch (error) {
     console.log(error);
 
