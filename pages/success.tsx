d6270a1261a9f080eabbdccde1788723307c27a7
Greg Yaniw <yaniw@rushstreetinteractive.com>
2022-09-06 12:12:25 -0400

    test

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,10 +1,7 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import { useEffect } from 'react'
-import styles from '../styles/Home.module.css'
 
 const Success: NextPage = () => {
-
   useEffect(() => {
     window.close();
   }, []);
